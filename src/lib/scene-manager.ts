8e0a5bca4b690bb82b80c526753680a56c8f2463
Ilya Siganov <ilya.blan4@gmail.com>
2024-10-05 16:35:59 +0600

    feat: add libs

---
+++ src/lib/scene-manager.ts
@@ -0,0 +1,43 @@
+import type { IUpdateable } from "./interfaces/updateable";
+
+export interface IScene {
+  activate(): void;
+  update(dt: number): void;
+  exit(): void;
+}
+
+export class SceneManager implements IUpdateable {
+  private current = "";
+  private next = "";
+  private scenes = new Map<string, IScene>();
+
+  get currentName() {
+    return this.current;
+  }
+
+  update(dt: number) {
+    if (this.current !== this.next) {
+      console.log(`Transition from '${this.current}' to '${this.next}'`);
+      this.scenes.get(this.current)?.exit();
+      this.current = this.next;
+      this.scenes.get(this.current)?.activate();
+    }
+
+    const sc = this.scenes.get(this.current);
+    if (sc) {
+      sc.update(dt);
+    } else {
+      console.error(`[SceneManager.update] unknown scene '${this.current}'`);
+    }
+  }
+
+  put(name: string, scene: IScene) {
+    this.scenes.set(name, scene);
+  }
+
+  set(scene: string) {
+    this.next = scene;
+  }
+}
+
+export const sceneManager = new SceneManager();
