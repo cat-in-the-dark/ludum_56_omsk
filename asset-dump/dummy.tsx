78f39a01754d85fe05d05c77e580c03da5f0311b
Anton Petrochenko @ Local <mtd@cardboardbox.ru>
2024-10-06 13:33:24 +0600

    feat: level 1

---
+++ asset-dump/dummy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="dummy" tilewidth="8" tileheight="8" tilecount="9" columns="3">
+ <image source="../public/assets/player1/1.png" width="24" height="24"/>
+</tileset>
