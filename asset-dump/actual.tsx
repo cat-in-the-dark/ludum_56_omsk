bd7876212e22dfbcaf6c12c75dac6827b479352a
Anton Petrochenko @ Local <mtd@cardboardbox.ru>
2024-10-06 14:52:10 +0600

    feat: title

--- asset-dump/actual.tsx
+++ asset-dump/actual.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="actual" tilewidth="8" tileheight="8" tilecount="64" columns="8">
  <image source="actual.png" width="64" height="64"/>
+ <tile id="37" probability="0.01"/>
+ <tile id="38" probability="0.01"/>
+ <tile id="39" probability="0.01"/>
+ <tile id="44" probability="0.01"/>
+ <tile id="45" probability="0.01"/>
+ <tile id="46" probability="0.01"/>
+ <tile id="47" probability="0.01"/>
 </tileset>
