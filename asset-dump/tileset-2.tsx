cbc96cfd2fdfab040df2b9e432011686abe4ae8a
Anton Petrochenko @ Local <mtd@cardboardbox.ru>
2024-10-06 14:39:04 +0600

    feat: levels

---
+++ asset-dump/tileset-2.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset-2" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="tileset-2.png" width="64" height="64"/>
+ <tile id="36" probability="2"/>
+ <tile id="37" probability="0.1"/>
+ <tile id="38" probability="0.1"/>
+ <tile id="39" probability="0.1"/>
+ <tile id="44" probability="0.1"/>
+ <tile id="45" probability="0.1"/>
+ <tile id="46" probability="0.1"/>
+ <tile id="47" probability="0.1"/>
+</tileset>
